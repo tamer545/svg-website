083970f67164ef3e50cf44c388bf2ac9e9f23f27
tamer545 <timo.angst@lernende.bbw.ch>
2023-04-09 20:44:06 +0200

    feat: add animated rgb background to sources

--- src/SourcesGroup.tsx
+++ src/SourcesGroup.tsx
@@ -6,19 +6,19 @@ function SourcesGroup() {
                 <ListGroup.Item>
                     <h1>Sources</h1>
                 </ListGroup.Item>
-                <ListGroup.Item action href="https://react-bootstrap.github.io/components/alerts">
+                <ListGroup.Item action href="https://react-bootstrap.github.io/components/alerts" className={"glow-on-hover"}>
                     Common Components (React Bootstrap Default Page)
                 </ListGroup.Item>
-                <ListGroup.Item action href="https://codemyui.com/parallax-pixel-stars-using-pure-css/">
+                <ListGroup.Item action href="https://codemyui.com/parallax-pixel-stars-using-pure-css/" className={"glow-on-hover"}>
                     Stars Background (CodeMyui)
                 </ListGroup.Item>
-                <ListGroup.Item action href="https://www.svgrepo.com/">
+                <ListGroup.Item action href="https://www.svgrepo.com/" className={"glow-on-hover"}>
                     Pre-made SVGs (SVGRepo)
                 </ListGroup.Item>
-                <ListGroup.Item action href="https://chat.openai.com/chat">
+                <ListGroup.Item action href="https://chat.openai.com/chat" className={"glow-on-hover"}>
                     Texts (ChatGPT)
                 </ListGroup.Item>
-                <ListGroup.Item action href="https://stablediffusionweb.com/#demo">
+                <ListGroup.Item action href="https://stablediffusionweb.com/#demo" className={"glow-on-hover"}>
                     Images (StableDiffusion)
                 </ListGroup.Item>
             </ListGroup>
